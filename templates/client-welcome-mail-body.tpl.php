a7d6ef56acbcabc52320475e18fda17bbb39e9b3
Teody C. Seguin <teodyseguin@gmail.com>
2021-03-26 10:19:18 +0800

    Initial commit

---
+++ templates/client-welcome-mail-body.tpl.php
@@ -0,0 +1,42 @@
+<h1 style="font-family: arial; text-align: center;">
+  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+</h1>
+
+<p style="font-family: arial; font-size: 18px; text-align: justify;">
+  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+</p>
+
+<p style="font-family: arial; font-size: 18px; text-align: justify;">
+  And now, you're on our list to receive our exclusive updates,
+  including special offers, the latest organic must-haves, go-to
+  gifts for your loved ones and more.
+</p>
+
+<p style="font-family: arial; font-size: 18px; text-align: center;">
+  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+</p>
+
+<div style="color: white; display: block; background-color: #8dc73f; width: 150px; margin: 30px auto 0; padding: 10px; text-align: center;">
+  <a href="<?php print $domain_path; ?>" style="color: white; text-decoration: none; font-family: arial; font-size: 20px; font-weight: bold;">
+    SHOP NOW
+  </a>
+</div>
+
+<p style="margin-top: 30px; text-align: center; font-family: arial; font-weight: bold;">P.S. Stay connected with us</p>
+<ul style="list-style: none; text-align: center; margin: 0; padding: 0;">
+  <li style="display: inline; margin: 0 10px;">
+    <a href="https://www.instagram.com/client_harvest" style="text-decoration: none;">
+      <img src="<?php print $images_path; ?>/instagram.png" />
+    </a>
+  </li>
+  <li style="display: inline; margin: 0 10px;">
+    <a href="https://www.facebook.com/Client-880444468802417" style="text-decoration: none;">
+      <img src="<?php print $images_path; ?>/facebook.png" />
+    </a>
+  </li>
+  <li style="display: inline; margin: 0 10px;">
+    <a href="https://twitter.com/clientharvest" style="text-decoration: none;">
+      <img src="<?php print $images_path; ?>/twitter.png" />
+    </a>
+  </li>
+</ul>
